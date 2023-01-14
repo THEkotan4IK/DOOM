fcaa19f679c9acdb433c7837a7431b21f65bacbd
THEkotan4IK <ikulikov.1307@gmail.com>
2023-01-14 13:14:06 +0300

    Project

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [Header("OPTIONS")]
+    public float range;
+    public float fireRate;
+    public float damage;
+    private float nextFire;
+
+    public float recoilX;
+    public float recoilY;
+
+    public int AmmoCount;
+    public int MagazineAmmoCount;
+    public int MaxMagazineAmmoCount;
+
+    public float deleteImpactTime;
+
+    [Header("GAMEOBJECTS")]
+    public GameObject camera;
+    public GameObject hitEffect;
+
+    [Header("AUDIO")]
+    public AudioSource shootSFX;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire && MagazineAmmoCount > 0)
+        {
+            RayShoot();
+            nextFire = Time.time + 0.1f / fireRate;
+        }
+    }
+
+    void RayShoot()
+    {
+        if (!shootSFX.isPlaying)
+        {
+            shootSFX.Play();
+        }
+        MagazineAmmoCount--;
+
+        camera.transform.rotation = camera.transform.rotation * Quaternion.Euler(UnityEngine.Random.Range(-recoilX, recoilX), UnityEngine.Random.Range(-recoilY, recoilY), 0);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.collider.name);
+            GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, deleteImpactTime);
+        }
+    }
+}
